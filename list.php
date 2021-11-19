dd69619e1e4f7af8098c93b595ba5ac5e5e725d3
dadoka84 <dadoka84@hotmail.com>
2021-11-19 18:13:47 +0100

    korekcija

--- list.php
+++ list.php
@@ -9,7 +9,7 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
   
-    echo '<div class="proizvod">', '<img src='.$row["slika"].' width="100%" height="200px">', '<br>' , '<div class="naziv">'. $row["boja"]," " . $row["velicina"]." " . $row["vrsta"]. "</div>  " , '<div class="cijena">'. $row["cijena"].'KM</div>','<br><button class="dugme"><a href="index.php">U košaricu</a></button><button><a href="index.php">Kupi proizvod</a></button>', '</div>';
+    echo '<div class="proizvod">', '<img src='.$row["slika"].' width="100%" height="200px">', '<br>' , '<div class="naziv">'. $row["boja"]," " . $row["velicina"]." " . $row["vrsta"]. "</div>  " , '<div class="cijena">'. $row["cijena"].'KM</div>','<br><button class="dugme"><a href="index.php">U košaricu</a></button><button><a href="index.php">Odaberi proizvod</a></button>', '</div>';
   }
 } else {
   echo "0 results";
